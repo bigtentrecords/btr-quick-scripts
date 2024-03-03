b3b7f6202cc149d143fe4cd21b772854d4f3e944
Nick DeMasi <nick@bigtentrecords.us>
2024-03-02 23:47:10 -0800

    default to WILTTM playlist if no id provided

--- download_WILTLM_album_art.py
+++ download_WILTLM_album_art.py
@@ -1,20 +1,56 @@
 """"Script to download album art for tracks in WILTLM playlist"""
+
 import os
 import sys
-from typing import Optional
 
+from dotenv import load_dotenv
 import requests
 import spotipy
 from spotipy.client import Spotify as SpotifyClient
 from spotipy.oauth2 import SpotifyOAuth
 
 
-DL_FOLDER_PATH_ROOT="/Users/nickdemasi/Mirror/Big Tent/Projects/Series/WILTLM"
+DL_FOLDER_PATH_ROOT = "/Users/nickdemasi/Mirror/Big Tent/Projects/Series/WILTLM"
+WILTTM_PLAYLIST_ID: str = "4xjL4vPqFFTb9hEtxbbirJ"
+
+
+def format_dl_folder_path(month: str) -> str:
+    """Formats folder name and path where WILTLM album art will be written"""
+    dl_folder_name = f"{month}_WILTLM"
+    dl_folder_path = f"{DL_FOLDER_PATH_ROOT}/{dl_folder_name}"
+    return dl_folder_path
+
+
+def get_playlist_tracks(sc: SpotifyClient, playlist_id: str) -> list[dict]:
+    """Pings Spotify Web API for a playlist object and collects track objects"""
+    playlist_tracks = [
+        itm["track"] for itm in sc.playlist(playlist_id)["tracks"]["items"]
+    ]
+    return playlist_tracks
+
+
+def download_album_art(track: dict, destination: str) -> int:
+    """Downloads album art from track objects to destination folder"""
+    # unpack track object
+    track_name = "_".join(track["name"].split(" "))
+    artist_name = "_".join(track["artists"][0]["name"].split(" "))
+    img_url = track["album"]["images"][0]["url"]
+
+    # download image
+    img_data = requests.get(img_url).content
+    with open(f"{destination}/covers/{artist_name}-{track_name}.jpg", "wb") as handler:
+        handler.write(img_data)
+
+    return 0
 
 
 def main(args):
     # unpack args
-    month, playlist_id = args
+    month = args[0]
+    try:
+        playlist_id = args[1]
+    except:
+        playlist_id = WILTTM_PLAYLIST_ID
 
     # connect to api
     scope = "user-library-read"
@@ -38,41 +74,12 @@ def main(args):
 
     # download album art
     tracks = get_playlist_tracks(sc, playlist_id)
-    for trk in tracks:
+    for i, trk in enumerate(tracks):
         download_album_art(trk, dl_folder_path)
-
-
-def format_dl_folder_path(month: str) -> str:
-    """Formats folder name and path where WILTLM album art will be written"""
-    dl_folder_name = f"{month}_WILTLM"
-    dl_folder_path = f"{DL_FOLDER_PATH_ROOT}/{dl_folder_name}"
-    return dl_folder_path
-
-
-def get_playlist_tracks(sc: SpotifyClient, playlist_id: str) -> list[dict]:
-    """Pings Spotify Web API for a playlist object and collects track objects"""
-    playlist_tracks = [
-        itm["track"]
-        for itm in sc.playlist(playlist_id)["tracks"]["items"]
-    ]
-    return playlist_tracks
-
-
-def download_album_art(track: dict, destination: str) -> int:
-    """Downloads album art from track objects to destination folder"""
-    # unpack track object
-    track_name = "_".join(track["name"].split(" "))
-    artist_name = "_".join(track["artists"][0]["name"].split(" "))
-    img_url = track["album"]["images"][0]["url"]
-
-    # download image
-    img_data = requests.get(img_url).content
-    with open(f"{destination}/covers/{artist_name}-{track_name}.jpg", 'wb') as handler:
-        handler.write(img_data)
-
-    return 0
+        print(f"Downloaded {i} of {len(tracks)} covers")
 
 
 if __name__ == "__main__":
+    load_dotenv()
     args = sys.argv[1:]
     main(args)
